6741c336866f0675e449e745959ce1180e30b255
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2022-12-18 21:25:22 +0300

    Added Tests and Logic

--- BinaryGapLib/BinaryConverter.cs
+++ BinaryGapLib/BinaryConverter.cs
@@ -1,10 +1,55 @@
-﻿namespace BinaryGapLib
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BinaryGapLib
 {
     public class BinaryConverter
     {
-        public BinaryConverter()
-        {
+        private uint _binaryGap;
+        private int _binaryGapsAmount = 0;
 
+        public int BinaryGapsAmount(uint integer)
+        {
+            string[] binary = ConvertToBinary(integer);
+            foreach (var bin in binary)
+            {
+                if (bin.Contains('0'))
+                {
+                    _binaryGapsAmount++;
+                }
+            }
+            return _binaryGapsAmount;
+        }
+
+        public uint CalculateZerosInBinaryGap(uint integer)
+        {
+            _binaryGapsAmount = BinaryGapsAmount(integer);
+            string[] binary = ConvertToBinary(integer);
+
+            if (_binaryGapsAmount == 1)
+            {
+                foreach (var bin in binary)
+                {
+                    if (bin.Contains('0'))
+                    {
+                        _binaryGap = (uint)bin.Length;
+                    }
+                    else
+                        continue;
+                }
+            }
+            else if (_binaryGapsAmount > 1)
+            {
+
+            }
+            else
+                _binaryGap = 0;
+            return _binaryGap;
+        }
+
+        private static string[] ConvertToBinary(uint integer)
+        {
+            return Convert.ToString(integer, 2).Trim('0').Split(new[] { '1' });
         }
     }
 }
--- BinaryGapTests/BinaryTests.cs
+++ BinaryGapTests/BinaryTests.cs
@@ -5,6 +5,7 @@ namespace BinaryGapTests
     public class Tests
     {
         private BinaryConverter _binaryConverter;
+        private uint _integer;
 
         [SetUp]
         public void Setup()
@@ -13,9 +14,30 @@ namespace BinaryGapTests
         }
 
         [Test]
-        public void Test1()
+        public void CheckTheBinaryGapOfASmallDigit()
         {
-            Assert.Pass();
+            _integer = 9;
+
+            Assert.That(_binaryConverter.BinaryGapsAmount(_integer), Is.EqualTo(1));
+            Assert.That(_binaryConverter.CalculateZerosInBinaryGap(_integer), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CheckTheBinaryGapOfABigNumber()
+        {
+            _integer = 20;
+
+            Assert.That(_binaryConverter.BinaryGapsAmount(_integer), Is.EqualTo(1));
+            Assert.That(_binaryConverter.CalculateZerosInBinaryGap(_integer), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CheckTheBinaryGapOfABinaryWithOnesOnly()
+        {
+            _integer = 15;
+
+            Assert.That(_binaryConverter.BinaryGapsAmount(_integer), Is.EqualTo(0));
+            Assert.That(_binaryConverter.CalculateZerosInBinaryGap(_integer), Is.EqualTo(0));
         }
     }
 }
